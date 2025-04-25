71860a55ed599ec80e1a3a629c849332725da09a
beya <jouinibeyai@gmail.com>
2025-04-25 15:11:37 -0400

    Final exam fix

--- WholeTemperatureCheckerFix.java
+++ WholeTemperatureCheckerFix.java
@@ -1,24 +1,37 @@
 import java.util.ArrayList;
 import java.util.Collections;
 /**
- * Write a description of class TemperatureChecker25 here.
+ * Write a description of class WholeTemperatureCheckerFix here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
-public class TemperatureChecker25
+public class WholeTemperatureCheckerFix
 {
     // instance variables - replace the example below with your own
     private int x;
-    
+
     /**
-     * Constructor for objects of class TemperatureChecker25
+     * Constructor for objects of class WholeTemperatureCheckerFix
      */
-    public TemperatureChecker25()
+    public WholeTemperatureCheckerFix()
     {
         // initialise instance variables
         x = 0;
     }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+    
 
     public static void
     checkTemperatures (ArrayList<Double>temperatures){
